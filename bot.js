51d5a2fad495796d1f1645db13de95a845237ee9
Sakayase <simon.ponitzki@gmail.com>
2021-10-05 04:20:12 +0200

    Base de bot

---
+++ bot.js
@@ -0,0 +1,17 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { token } = require("./config.json")
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(token);
